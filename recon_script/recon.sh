8bfc25109943523c74fa95ca3dcde6d4d05a1f36
Sam Ebison <ebsa491@gmail.com>
2021-02-09 20:25:30 +0330

    logging and tools

--- recon_script/recon.sh
+++ recon_script/recon.sh
@@ -8,10 +8,31 @@
 export RED="\033[1;31m"
 export GREEN="\033[1;32m"
 export BLUE="\033[1;36m"
+export YELLOW="\033[1;33m"
 export PURPLE="\033[1;35m"
 export NOPE="\033[0m"
 
+export seclists_path='~/wordlists/SecLists/' # Don't forget / at the end of this variable
+
 # ##################################################
+
+# ################### TOOLS #######################
+# - Sublist3r
+# - crt.sh
+# - waybackurls
+# - dirsearch
+# - nikto
+# - Knockpy
+# - webscreenshot
+# - unfurl
+# - httprobe
+# - ffuf (maybe, I'm not sure :/)
+# - c99 subdomain finder (maybe, I'm not sure :/)
+# - Asnlookup (maybe, I'm not sure :/)
+# - virtual-host-discovery (maybe, I'm not sure :/)
+# - massdns (maybe, I'm not sure :/)
+# - aquatone (maybe, I'm not sure :/)
+# ################################################
 
 echo -e "$BLUE
 ██████╗ ███████╗ ██████╗ ██████╗ ███╗   ██╗██╗
@@ -22,10 +43,21 @@ echo -e "$BLUE
 ╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝
 $NOPE"
 
-usage() { echo "USAGE: ./recon.sh TOOLS_PATH";exit 1; }
+usage() {
+    echo -e "\vUSAGE: ./recon.sh TOOLS_PATH\vExample: ./recon.sh ~/tools/"
+    exit 1
+}
 
 error() {
     echo -e "[$RED-$NOPE] $1"
+}
+
+warning() {
+    echo -e "[$YELLOW!$NOPE] $1"
+}
+
+log() {
+    echo -e "[$GREEN+$NOPE] $1"
 }
 
 if [[  $# != 1  ]];then
