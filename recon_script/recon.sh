cf3985084c40a6c0ca533a35293442c97e05ac00
Sam Ebison <ebsa491@gmail.com>
2021-02-10 14:07:19 +0330

    tools functions

--- recon_script/recon.sh
+++ recon_script/recon.sh
@@ -23,14 +23,13 @@ export seclists_path='~/wordlists/SecLists/' # Don't forget / at the end of this
 # - dirsearch
 # - nikto
 # - Knockpy
-# - webscreenshot
+# - massdns
 # - unfurl
 # - httprobe
+# - Asnlookup
+# - virtual-host-discovery
 # - ffuf (maybe, I'm not sure :/)
 # - c99 subdomain finder (maybe, I'm not sure :/)
-# - Asnlookup (maybe, I'm not sure :/)
-# - virtual-host-discovery (maybe, I'm not sure :/)
-# - massdns (maybe, I'm not sure :/)
 # - aquatone (maybe, I'm not sure :/)
 # ################################################
 
@@ -44,7 +43,7 @@ echo -e "$BLUE
 $NOPE"
 
 usage() {
-    echo -e "\vUSAGE: ./recon.sh TOOLS_PATH\vExample: ./recon.sh ~/tools/"
+    echo -e "\vUSAGE: ./recon.sh TOOLS_PATH\vExample: ./recon.sh ~/tools(without /)"
     exit 1
 }
 
@@ -64,5 +63,60 @@ if [[  $# != 1  ]];then
     usage
 else
     export tools_path="$1"
+    export report_date="$(date +%d_%m_%Y-%H.%M)"
+    mkdir -p recon/$report_date/
+    export report_path="recon/$report_date"
 fi
+
+# Sublist3r
+sublist3r() {
+    $tools_path/Sublist3r/sublist3r.py -d $1 -t 5 > $report_path/$1/sublister.txt
+}
+
+# crt.sh
+crtsh() {
+    $tools_path/massdns/scripts/ct.py $1 2>/dev/null > $report_path/$1/tmp.txt
+    [ -s $report_path/$1/tmp.txt ] && cat $report_path/$1/tmp.txt | $tools_path/massdns/bin/massdns -r $tools_path/massdns/lists/resolvers.txt -t A -q -o S -w  $report_path/$1/crtsh.txt
+    cat $report_path/$1/subdomains.txt | $tools_path/massdns/bin/massdns -r $tools_path/massdns/lists/resolvers.txt -t A -q -o S -w  $report_path/$1/domaintemp.txt
+}
+
+# waybackurls
+waybackurls() {
+    cat $report_path/$1/urls.txt | waybackurls > $report_path/$1/wayback/waybackurls.txt
+    cat $report_path/$1/wayback/waybackurls.txt | sort -u | unfurl --unique keys > $report_path/$1/wayback/paramlist.txt
+    [ -s $report_path/$1/wayback/paramlist.txt ] && log "$report_path/$1/wayback/paramlist.txt saved"
+}
+
+# dirsearch
+dirsearch() {
+    cat $report_path/$1/urls.txt | xargs -P10 -I % sh -c "python3 $tools_path/dirsearch/dirsearch.py -e php,asp,aspx,jsp,html,zip,jar -w $tools_path/dirsearch/db/dicc.txt -t 30 -u %" > $report_path/$1/dirsearch.txt
+}
+
+# nikto
+nikto() {
+    for target in $(cat $report_path/$1/urls.txt);do
+        echo -e " $PURPLE=-=-=-=$YELLOW $target $PURPLE=-=-=-=$NOPE " >> $report_path/$1/nikto.txt
+        nikto -h $target >> $report_path/$1/nikto.txt
+    done
+}
+
+# knockpy
+knockpy() {
+    knockpy $1 -j > $report_path/$1/knock.txt
+}
+
+# virtual-host-discovery
+vhostdiscovery() {
+    ruby $tools_path/virtual-host-discovery/scan.rb --ip=$1 --host=domain.tld
+}
+
+# Asnlookup
+asnlookup() {
+    python3 $tools_path/Asnlookup/asnlookup.py -n "-A -T4" -o "$1"
+}
+
+# amass
+amassf() {
+    amass enum -d $1
+}
 
